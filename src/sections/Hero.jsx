cb68de931f768bf1e4520520db1e7b305361f87c
Siddiqui Mamoon Ahmad <112717860+Mamoon-5G@users.noreply.github.com>
2026-08-24 22:09:14 +0530

    update twitter handle

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -103,7 +103,7 @@ export const Hero = memo(() => {
         { icon: Github, href: "https://github.com/Mamoon-5G", label: "GitHub Profile" },
         { icon: Linkedin, href: "https://linkedin.com/in/mamoon-siddiquii", label: "LinkedIn Profile" },
         { icon: Instagram, href: "https://instagram.com/m_a_siddiqui_5g", label: "Instagram Profile" },
-        { icon: Twitter, href: "https://twitter.com/mamoon_4g", label: "Twitter Profile" },
+        { icon: Twitter, href: "https://twitter.com/MamoonAhmadSidd", label: "Twitter Profile" },
         { icon: Code2, href: "https://leetcode.com/u/Mamoon-5G", label: "LeetCode Profile" }
     ], []);
 
